d994e28e800bf8711439bbd4d138074769ad14df
Charles K. Neimog <charlesneimog@outlook.com>
2023-05-03 16:31:47 -0300

    coloquei as coisas de permisoes em um arquivo separado

---
+++ public/permitionRequests.js
@@ -0,0 +1,50 @@
+// =====================================================
+// It requests fullscreen mode
+// =====================================================
+document.onclick = function (argument) {
+    var conf = confirm("Fullscreen mode?");
+    var docelem = document.documentElement;
+    if (conf == true) {
+        if (docelem.requestFullscreen) {
+            docelem.requestFullscreen();
+            // disable this function
+            document.onclick = null;
+        }
+        else if (docelem.mozRequestFullScreen) {
+            docelem.mozRequestFullScreen();
+            document.onclick = null;
+        }
+        else if (docelem.webkitRequestFullScreen) {
+            docelem.webkitRequestFullScreen();
+            document.onclick = null;
+        }
+        else if (docelem.msRequestFullscreen) {
+            docelem.msRequestFullscreen();
+            document.onclick = null;
+        }
+    }
+}
+
+// =====================================================
+// Play simple video in order to not turn off the screen
+// =====================================================
+var promise = document.querySelector('video').play();
+var Util={};
+Util.base64 = function(mimeType, base64) {
+return 'data:' + mimeType + ';base64,' + base64;
+};
+var video = document.getElementById('screenOn');
+video.setAttribute('loop', '');
+video.setAttribute('autoplay', '');
+function addSourceToVideo(element, type, dataURI) {
+    var source = document.createElement('source');
+    source.src = dataURI;
+    source.type = 'video/' + type;
+    element.appendChild(source);
+}
+addSourceToVideo(video,'webm', Util.base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
+addSourceToVideo(video, 'mp4', Util.base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+video.play();
+// =====================================================
+// =====================================================
+// =====================================================
---
+++ score.py
@@ -0,0 +1,98 @@
+from random import randint
+import os
+os.environ['QT_QPA_PLATFORM'] = "xcb" 
+if os.name == 'nt':
+    os.environ['QT_QPA_PLATFORM'] = "windows"
+    
+
+from neoscore.common import *
+
+
+def getpitchKey(pitch, cents=0):
+    note = {
+        # natural
+        'c': ['c', ''],
+        'd': ['d', ''],
+        'e': ['e', ''],
+        'f': ['f', ''],
+        'g': ['g', ''],
+        'a': ['a', ''],
+        'b': ['b', ''],
+        # sharp
+        'c#': ['c', 'accidentalSharp'],
+        'd#': ['d', 'accidentalSharp'],
+        'e#': ['e', 'accidentalSharp'],
+        'f#': ['f', 'accidentalSharp'],
+        'g#': ['g', 'accidentalSharp'],
+        'a#': ['a', 'accidentalSharp'],
+        'b#': ['b', 'accidentalSharp'],
+        # flat
+        'cb': ['c', 'accidentalFlat'],
+        'db': ['d', 'accidentalFlat'],
+        'eb': ['e', 'accidentalFlat'],
+        'fb': ['f', 'accidentalFlat'],
+        'gb': ['g', 'accidentalFlat'],
+        'ab': ['a', 'accidentalFlat'],
+        'bb': ['b', 'accidentalFlat'],
+    }
+    return note[pitch]
+
+
+def chord(pitches):
+    try:
+        neoscore.shutdown()
+    except BaseException:
+        pass
+    neoscore.setup()
+
+    if isinstance(pitches, str):
+        pitches = [pitches]
+
+    # make all string with small letters
+    pitches = [x.lower() for x in pitches]
+
+    py4pdTMPfolder = "./"
+    for file in py4pdTMPfolder:
+        if file.endswith(".ppm"):
+            try:
+                os.remove(py4pdTMPfolder + "/" + file)
+            except BaseException:
+                pass
+    staffSoprano = Staff((Mm(0), Mm(0)), None, Mm(30))
+    trebleClef = 'treble'
+    Clef(ZERO, staffSoprano, trebleClef)
+    staffBaixo = Staff((ZERO, Mm(20)), None, Mm(30))
+    bassClef = 'bass'
+    Clef(ZERO, staffBaixo, bassClef)
+    Path.rect((Mm(-10), Mm(-10)), None, Mm(42), Mm(42),
+              Brush(Color(255, 255, 255, 0)), Pen(thickness=Mm(0)))
+    silaba = "Ky"
+    text = f"""
+<p>
+    <b>{silaba}</b>
+</p>
+"""
+    default_font = neoscore.default_font # Alias just for docs legibility
+    RichText(Point(x=Unit(13.0), y=Unit(30.0)), staffSoprano, text, Inch(1), default_font)
+
+
+    for pitch in pitches:
+        # in pitch remove not number
+        pitchWithoutNumber = pitch.replace(pitch[-1], '')
+        pitchOctave = int(pitch[-1])
+        pitchClass, accidental = getpitchKey(pitchWithoutNumber)
+        note = [(pitchClass, accidental, pitchOctave)]
+        if pitchOctave < 4:
+            Chordrest(Mm(5), staffBaixo, note, (int(1), int(1)))
+        else:
+            Chordrest(Mm(5), staffSoprano, note, (int(1), int(1)))
+    randomNumber = randint(1, 100)
+    notePathName = py4pdTMPfolder + "/" + pitch + f"{randomNumber}.png"
+    neoscore.render_image(rect=None, dest=notePathName, dpi=1500, wait=True)
+    neoscore.shutdown()
+    if os.name == 'nt':
+        notePathName = notePathName.replace("\\", "/")
+    return None
+
+
+chord(["c4"])
