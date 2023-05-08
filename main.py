88423a79dc9f7bd4a8fd5205deef1fed2dd75a5a
Charles K. Neimog <charlesneimog@outlook.com>
2023-05-07 23:05:12 -0300

    feat(main.py): add function to save start time and load it as a Pydon object in Pydon's environment

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+import os
+import time
+import pd
+
+def saveTimeStart(start):
+    home = pd.home()
+    pd.print(home)
+
+
+def py4pdLoadObjects():
+    pd.addobject(saveTimeStart, 'setStart')
+
+
+
+
