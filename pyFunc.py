65351ad5bc6d4c96d7b037dbee2a38760eb02f15
Charles K. Neimog <charlesneimog@outlook.com>
2023-05-07 23:05:32 -0300

    refactor(pyFunc.py): rename setStartTime parameter to delayInSeconds to improve semantics feat(pyFunc.py): add resetStartTime function to reset the start time of connected devices docs(pyFunc.py): add docstrings to setStartTime and resetStartTime functions

--- pyFunc.py
+++ pyFunc.py
@@ -3,11 +3,19 @@ import os
 import time
 import pd
 
-def setStartTime(start):
-    return start
+def setStartTime(delayInSeconds):
+    """ It saves (in public/start.json) the time that all connected devices should start to run."""
+    startTime = time.time() + delayInSeconds
+    home = pd.home()
+    with open(home + "/public/start.json", "w") as f:
+        json.dump({"startTime": startTime}, f)
 
+    
+def resetStartTime():
+    """ It saves (in public/start.json) the time that all connected devices should start to run."""
+    home = pd.home()
+    with open(home + "/public/start.json", "w") as f:
+        json.dump({"startTime": 0}, f)
 
-def py4pdLoadObjects():
-    pd.addobject(setStartTime, "setStartTime")
-
+    
 
