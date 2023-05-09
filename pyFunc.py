6224096f684ce59d6251ec7237379cf31858656c
Charles K. Neimog <charlesneimog@outlook.com>
2023-05-09 15:56:16 -0300

    Add qrcode generator for PureData

--- pyFunc.py
+++ pyFunc.py
@@ -1,7 +1,11 @@
+import pd
+
 import json
 import os
 import time
-import pd
+import qrcode
+import socket
+
 
 def setStartTime(delayInSeconds):
     """ It saves (in public/start.json) the time that all connected devices should start to run."""
@@ -16,7 +20,16 @@ def resetStartTime():
     home = pd.home()
     with open(home + "/public/start.json", "w") as f:
         json.dump({"startTime": 0}, f)
+        
+def mkQrCode():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    img = qrcode.make("https://" + ip_address + ":2023")
+    home = pd.home() + "/public/qrcode.png"
+    img.save(home)
+    pd.show(home)
 
 def py4pdLoadObjects():
     pd.addobject(resetStartTime, "resetTime")
     pd.addobject(setStartTime, "setStartTime")
+    pd.addobject(mkQrCode, "mkqrcode", objtype="VIS")
